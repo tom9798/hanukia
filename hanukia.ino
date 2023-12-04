9163784be67f5831031fd7e0232389f8b1a155fe
tom9798 <soustieltom@gmail.com>
2023-12-04 15:12:11 +0200

    no standby mode

--- hanukia.ino
+++ hanukia.ino
@@ -1,7 +1,7 @@
 #include "LowPower.h"
 const int ButtonPin = 13;
-const int wake = 2; //Interrupt pin for waking up from stdby mode (MUST BE THIS ONE OR PIN 3!! current physical configuration - pin 2)
-const int LedPin[] = {3, 4, 5, 6, 7, 8, 9, 10, 11};  // Array of LED pins
+// const int wake = 2; //Interrupt pin for waking up from stdby mode (MUST BE THIS ONE OR PIN 3!! current physical configuration - pin 2)
+const int LedPin[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};  // Array of LED pins
 int CurrentDay = 0;                                 // Counter to keep track of the current day
 int debounceDuration = 120;                         // Time duration for button debounce
 bool isON = false;
@@ -18,14 +18,14 @@ void setup() {
     pinMode(LedPin[i], OUTPUT);
   }
   pinMode(ButtonPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(wake), wakeUp, CHANGE); // this pin is connected to the button pin, which will wake up the arduino from power saving
-  Serial.begin(9600);
+  // attachInterrupt(digitalPinToInterrupt(wake), wakeUp, CHANGE); // this pin is connected to the button pin, which will wake up the arduino from power saving
+  // Serial.begin(9600);
 }
 
-void wakeUp(){
-  // just a function to go to when waking up, no functionality needed
-  // technically possible to add something nice when starting, such as lighting the "shamash", up to you
-}
+// void wakeUp(){
+//   // just a function to go to when waking up, no functionality needed
+//   // technically possible to add something nice when starting, such as lighting the "shamash", up to you
+// }
 
 void loop() {
   // Serial.print("working\n"); //just for debugging, can check if the for loop stopped = stdby mode
@@ -37,9 +37,9 @@ void loop() {
       for (int i = 0; i < 10; i++) {
         ledStates[i] = false; // Reset LED states when turning off
       }
-      allOff = true;
+      // allOff = true;
     } else {
-      allOff = false;
+      // allOff = false;
       isON = true;
       timeON = millis();
       ledStates[CurrentDay - 1] = true; // Mark the LED that should be on
@@ -69,7 +69,7 @@ void loop() {
       ledStates[i] = false; // Reset LED states when turning off (if we comment out this for loop, we can set it to remember how many candles were lit instead of lighting tem all each time)
     } 
     turnLED(CurrentDay);
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); // set arduino to power saving mode
+    // LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); // set arduino to power saving mode
   }
 
   // Add the flame flicker effect
